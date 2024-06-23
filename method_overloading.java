bad3e352a7021151d7912bc2eb06032f829891a6
Mohammad Sameer <155916546+MDS-Sameer012@users.noreply.github.com>
2024-06-23 21:05:55 +0000

    Method Overloading

---
+++ method_overloading.java
@@ -0,0 +1,20 @@
+/**
+ * method_overloading
+ */
+
+ class MathUtil{
+    public int add(int a, int b){
+        return a+b;
+    }
+    public double add(double a,double b){
+        return (double)(a+b);
+    }
+ }
+
+public class method_overloading {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
